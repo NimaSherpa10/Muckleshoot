20b282e3324915ba652e8c3a730bfe45276acbed
Nicholas Cutlip <62163487+cut-lip@users.noreply.github.com>
2023-04-15 10:40:36 -0700

    Update server.ts
    Add comments.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
+// Import express module
 const express = require('express');
 
+// Construct an express application
 const app = express();
 
+// Define callback function for HTTP GET requests to root '/'
 app.get('/', (request, response) => {
     response.send('Here we go!');
 });
 
-const port = 3000;
+const port = 3000;  // Assign port number to variable
+
+// Listen on port for connection request
 app.listen(port, () => {
     console.log('Server running at http://localhost:${port}');
 })
